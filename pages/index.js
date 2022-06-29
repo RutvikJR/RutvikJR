430525f6bd56ea3696ac9f98500be1f8325602a2
RutvikJR <rachhadiyarutvik09040@gmail.com>
2022-06-30 01:31:34 +0530

    next setup done

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <h1 className='text-blue-600'>hello</h1>
+  )
+}
